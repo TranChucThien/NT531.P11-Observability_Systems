fa36e9d182a5e94cd18e62cb8e741e9037f69c90
TonyQ2k3 <ntbquoc2015@gmail.com>
2024-10-10 20:54:10 +0700

    Added terraform infra

---
+++ infra/terraform/main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  token = var.token
+}
+
+locals {
+  prod_cluster_name = "production-environment"
+  prod_node_group_name = "production-nodes"
+  vpc_id = var.vpc_id
+  subnet_ids = ["subnet-09f008b76583d8cae", "subnet-0221a26c2daacbdd7", "subnet-0db67585071973542", "subnet-0352a68cb4f1a9b3b"]
+  iam_role_arn = var.iam_role_arn
+}
+
+# EKS Cluster provisioning
+resource "aws_eks_cluster" "production_cluster" {
+  name     = local.prod_cluster_name
+  role_arn = local.iam_role_arn
+
+  vpc_config {
+    subnet_ids = local.subnet_ids
+  }
+
+  version = "1.29" 
+}
+
+
+# EKS Node Group
+resource "aws_eks_node_group" "prod_node_group" {
+  cluster_name    = local.prod_cluster_name
+  node_group_name = local.prod_node_group_name
+  node_role_arn    = local.iam_role_arn
+  subnet_ids       = local.subnet_ids
+
+  scaling_config {
+    desired_size = 3
+    max_size     = 4
+    min_size     = 3
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [ aws_eks_cluster.production_cluster ]
+}
+
+
+# EKS Add-ons
+resource "aws_eks_addon" "coredns" {
+  cluster_name = local.prod_cluster_name
+  addon_name = "coredns"
+  addon_version = "v1.11.1-eksbuild.4"
+  depends_on = [ aws_eks_cluster.production_cluster, aws_eks_node_group.prod_node_group ]
+}
+
+resource "aws_eks_addon" "kube-proxy" {
+  cluster_name = local.prod_cluster_name
+  addon_name = "kube-proxy"
+  addon_version = "v1.29.0-eksbuild.1"
+  depends_on = [ aws_eks_cluster.production_cluster ]
+}
+
+resource "aws_eks_addon" "vpc-cni" {
+  cluster_name = local.prod_cluster_name
+  addon_name = "vpc-cni"
+  addon_version = "v1.16.0-eksbuild.1"
+  depends_on = [ aws_eks_cluster.production_cluster ]
+}
+
+resource "aws_eks_addon" "eks-pod-identity-agent" {
+  cluster_name = local.prod_cluster_name
+  addon_name = "eks-pod-identity-agent"
+  addon_version = "v1.2.0-eksbuild.1"
+  depends_on = [ aws_eks_cluster.production_cluster ]
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "access_key" {
+  description = "AWS Access Key"
+  default = "ASIAX32MPRGZNBBGVQMH"
+}
+
+variable "secret_key" {
+  description = "value"
+  default = "ExhTkkqjkJIZmuJmSfd1u6tn2bjdFFKolSqTxHAL"
+}
+
+variable "token" {
+  description = "value"
+  default = "IQoJb3JpZ2luX2VjEAgaCXVzLXdlc3QtMiJGMEQCIGN6bXuGLGGWNYBFpBzNs4/kULv6ro6CIZHGn+tHPs9rAiAJiynQdOFBHalHVMriOfSRIanN4DWuv0FTzc0Qp8iAuCqwAghgEAAaDDU0MDc4OTQxMDIyNiIMlvA2tdUy8aXaaHCrKo0CR0eiR60F+iFPaA2InysJBMDRpJIc4yN/KXFsnUBiq23UNivRNXsrbujzDsRbqPzEyEr6UfeeU53cdf6UZLTQbIUo7faXSgsM394bmuUMSj9bg1+MJ8MlhdzVrg9xkSCZUnyZuqoMYKMuCpGqqN3gLsdytja6E+9j1+BqEfeB7JoiLIv+/IOImxtAvyMl2CfLOzSeP45FV4J671n3R4Yn36p1/PZqDkLwhLIADG63L+t8s9YJ6mE+zquAsJMLAnw3eZzWWop86QJc9xjrJH89VD/eQVOybxQAqWRYPL7DxADQTMzeJJpAV1llXX8edK8mZ4xRwrmcDQCC+vBlOU8ZAVqT+jzW8ZRDOj+qRqwwlL6auAY6ngGJzrfjs0iWc+afzCv6INZ721JI8y2/lFZSoAHplaMkDU/tPdhv2z3Rs1RzP7CRoV2sbTGUIPtfQ0bSq9u3g+Yg8TV9f4ZhKMhWkhcDBf4pXScvBbx7oKP52+rFcxjvqeIqYUeaXxacSmYr45U7HMoqdpNiggRvDOdeam7TAtWP5kA0wiVnsWYmclTY4pQQQyPhDRAaouh7jLHfbTnluw=="
+}
+
+variable "iam_role_arn" {
+  description = "value"
+  default = "arn:aws:iam::540789410226:role/LabRole"
+}
+
+variable "vpc_id" {
+  description = "value"
+  default = "vpc-0d68568bd7918bfa2"
+}
+
